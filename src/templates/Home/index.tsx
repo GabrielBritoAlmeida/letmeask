568b6d6b9ced331e7c5adaf1b082404a96c5cdc6
Gabriel <gabrielfranccelino@gmail.com>
2021-06-26 17:58:39 -0300

    style: page home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,24 +1,45 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Grid, Image, Text, Flex } from '@chakra-ui/react'
 import illustrationImg from 'assets/images/illustration.svg'
 import logoImg from 'assets/images/logo.svg'
 import googleIconImg from 'assets/images/google-icon.svg'
 
 export function HomeTemplate() {
   return (
-    <Box w="100%" h="100vh">
-      <aside>
-        <img
+    <Grid w="100%" h="100vh" templateColumns="repeat(2, 1fr)">
+      <Box
+        as="aside"
+        flex="7"
+        bgColor="purple.600"
+        color="white"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        p="120px 80px"
+      >
+        <Image
+          maxW="320px"
           src={illustrationImg}
           alt="Ilustração simbolizando perguntas e respostas"
         />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
-      </aside>
-      <main>
-        <div>
-          <img src={logoImg} alt="Logo letmeask" />
+        <Text as="strong" fontSize="4xl">
+          Crie salas de Q&amp;A ao-vivo
+        </Text>
+        <Text fontSize="2xl">
+          Tire as dúvidas da sua audiência em tempo-real
+        </Text>
+      </Box>
+
+      <Flex as="main" align="center" justify="center" flex="8" p="0 32px">
+        <Flex
+          flexDirection="column"
+          w="100%"
+          maxW="320px"
+          align="stretch"
+          textAlign="center"
+        >
+          <Image src={logoImg} alt="Logo letmeask" alignSelf="center" />
           <button>
-            <img src={googleIconImg} alt="Logo do Google" />
+            <Image src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
 
@@ -28,8 +49,8 @@ export function HomeTemplate() {
             <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">Entrar na sala</button>
           </form>
-        </div>
-      </main>
-    </Box>
+        </Flex>
+      </Flex>
+    </Grid>
   )
 }
